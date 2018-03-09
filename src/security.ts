23522176f5eed993ef5cf8e18f4a602c793600c7
Bond <bond@max.ng>
2018-03-09 10:49:16 +0100

    Create a RaveSecurity class

--- src/security.ts
+++ src/security.ts
@@ -1,62 +1,24 @@
-import crypto from 'crypto';
+import RaveBase from './rave';
+import { encrypt, generateIntegrityHash } from './helpers/security';
 
-/**
- * Get an encryption key for encrypting transactions
- *
- * @param {string} secretKey Flutterwave Secret Key
- * @returns {string} a 24 byte hash
- */
-function deriveEncryptionKey(secretKey: string): string {
-  // Generate a 32 byte MD5 hash
-  const md5Hash = crypto
-    .createHash('md5')
-    .update(secretKey)
-    .digest('hex');
+class RaveSecurity {
+  private raveBase: RaveBase;
+  constructor(raveBase: RaveBase) {
+    this.raveBase = raveBase;
+  }
 
-  // Reduce the 32 byte hash to 12 bytes
-  const md5HashLast12 = md5Hash.substr(-12);
-
-  const strippedSecret = secretKey.replace('FLWSECK-', '');
-
-  // Get the first 12 byes of the secret key
-  const secretKeyFirst12 = strippedSecret.substr(0, 12);
-
-  /**
-   * Reduce the 256 bit hash to 192 bits
-   * in a deterministic fashion so we can use 3DES.
-   * Why not use AES and keep the 256 bit? ¯\_(ツ)_/¯
-   */
-  return secretKeyFirst12 + md5HashLast12;
-}
-
-/**
- * Encrypt a given payload with the supplied encryption key
- *
- * Uses 3DES encryption
- */
-export function encrypt(secretKey: string, payload: string) {
-  const encryptionKey = deriveEncryptionKey(secretKey);
-  const cipher = crypto.createCipheriv('des-ede3', encryptionKey, '');
-  const encrypted = cipher.update(payload, 'utf8', 'base64');
-
-  return encrypted + cipher.final('base64');
-}
-
-export function generateIntegrityHash(secretKey: string, payload: any) {
-  const keys = Object.keys(payload).sort();
-
-  const values = keys.reduce((list, key) => {
-    if (key === 'integrity_hash') {
-      return list;
+  async encrypt(data: string | any) {
+    let payload = data;
+    if (typeof payload !== 'string') {
+      payload = JSON.stringify(payload);
     }
 
-    return `${list}${payload[key]}`;
-  }, '');
+    return encrypt(this.raveBase.secretKey, payload);
+  }
 
-  const hashValue = `${values}${secretKey}`;
-
-  return crypto
-    .createHash('sha256')
-    .update(hashValue, 'utf8')
-    .digest('hex');
+  async generateIntegrityHash(payload: any) {
+    return generateIntegrityHash(this.raveBase.secretKey, payload);
+  }
 }
+
+export default RaveSecurity;
